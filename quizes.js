f2f8f2e6a738f1b14815354cdd4413fb84eb7e45
itsmenneka <nnekaxx@gmail.com>
2025-03-17 22:08:35 -0400

    Can now delete decks
    A delete button has been added to the decks. No more use for a deck? delete it with the cool new shiny delete button

--- quizes.js
+++ quizes.js
@@ -12,7 +12,18 @@ document.addEventListener("DOMContentLoaded", () => {
           deckName
         )}'">Open Deck</button>
         <button onclick="location.href='edit-deck.html?deck=${deckName}'">Edit Deck</button>
+        <button class="delete-deck-btn" data-deck="${deckName}">Delete Deck</button>
       `;
     decksContainer.appendChild(deckDiv);
+
+    // Add delete functionality for pre-made decks
+    deckDiv.querySelector(".delete-deck-btn").addEventListener("click", () => {
+      if (confirm(`Are you sure you want to delete the deck "${deckName}"?`)) {
+        delete decks[deckName]; // Remove the deck from the object
+        localStorage.setItem("decks", JSON.stringify(decks)); // Update localStorage
+        deckDiv.remove(); // Remove the deck from the UI
+        alert(`Deck "${deckName}" has been deleted.`);
+      }
+    });
   }
 });
